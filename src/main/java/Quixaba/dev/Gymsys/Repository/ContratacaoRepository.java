32f7791718665117fe39435ad7b021331f5d9b90
savioquixaba <savioquixaba1@hotmail.com>
2026-03-20 10:49:45 -0300

    feat: criado Repositorio da Entidade Contratacao

---
+++ src/main/java/Quixaba/dev/Gymsys/Repository/ContratacaoRepository.java
@@ -0,0 +1,7 @@
+package Quixaba.dev.Gymsys.Repository;
+
+import Quixaba.dev.Gymsys.Models.ContratacaoModel;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ContratacaoRepository extends JpaRepository<ContratacaoModel, Long> {
+}
