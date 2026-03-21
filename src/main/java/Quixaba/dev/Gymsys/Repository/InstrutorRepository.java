355591d3bd0e6829aed293fb5eca686139917d4e
savioquixaba <savioquixabaone@gmail.com>
2026-03-21 11:39:15 -0300

    chore: Adiciona Notacao de Repositorio as classes

--- src/main/java/Quixaba/dev/Gymsys/Repository/InstrutorRepository.java
+++ src/main/java/Quixaba/dev/Gymsys/Repository/InstrutorRepository.java
@@ -2,6 +2,9 @@ package Quixaba.dev.Gymsys.Repository;
 
 import Quixaba.dev.Gymsys.Models.InstrutorModel;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
 
 public interface InstrutorRepository extends JpaRepository<InstrutorModel, Long> {
 }
--- src/main/java/Quixaba/dev/Gymsys/Repository/PlanoRepository.java
+++ src/main/java/Quixaba/dev/Gymsys/Repository/PlanoRepository.java
@@ -2,6 +2,8 @@ package Quixaba.dev.Gymsys.Repository;
 
 import Quixaba.dev.Gymsys.Models.PlanoModel;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface PlanoRepository extends JpaRepository<PlanoModel, Long> {
 }
