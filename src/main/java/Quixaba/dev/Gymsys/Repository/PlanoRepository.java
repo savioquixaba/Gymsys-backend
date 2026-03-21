6a3b58fc9f27a03cc5a337b1791a15c922092dc4
savioquixaba <savioquixabaone@gmail.com>
2026-03-21 11:23:56 -0300

    feat: adiciona Repositorio da entidade Plano

---
+++ src/main/java/Quixaba/dev/Gymsys/Repository/PlanoRepository.java
@@ -0,0 +1,7 @@
+package Quixaba.dev.Gymsys.Repository;
+
+import Quixaba.dev.Gymsys.Models.PlanoModel;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PlanoRepository extends JpaRepository<PlanoModel, Long> {
+}
