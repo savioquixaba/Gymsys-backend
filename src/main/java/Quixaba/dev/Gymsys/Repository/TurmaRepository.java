c587197c7bed846fc7c4c54f0735542f93779056
savioquixaba <savioquixabaone@gmail.com>
2026-03-21 11:22:03 -0300

    feat: Adiciona Repositorio da Entidade Turma

---
+++ src/main/java/Quixaba/dev/Gymsys/Repository/TurmaRepository.java
@@ -0,0 +1,7 @@
+package Quixaba.dev.Gymsys.Repository;
+
+import Quixaba.dev.Gymsys.Models.TurmaModel;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TurmaRepository extends JpaRepository<TurmaModel, Long> {
+}
