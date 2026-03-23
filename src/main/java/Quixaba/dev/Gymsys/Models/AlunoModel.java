2cd5cbd6353b2c02c4a0f4ec713a5e329e1e13f7
savioquixaba <savioquixaba1@hotmail.com>
2026-03-23 14:04:42 -0300

    feat: adicionado beans de validacoes na classe AlunoModel

--- src/main/java/Quixaba/dev/Gymsys/Models/AlunoModel.java
+++ src/main/java/Quixaba/dev/Gymsys/Models/AlunoModel.java
@@ -1,6 +1,9 @@
 package Quixaba.dev.Gymsys.Models;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -19,9 +22,16 @@ public class AlunoModel {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    //Não permite espaços nem objetos nulos
+    @NotBlank
     private String nome;
+    @NotBlank
     private String cpf;
+    @NotBlank
     private String matricula;
+
+    @NotNull
     private LocalDate dataNascimento;
 
     @OneToMany(mappedBy = "aluno")
