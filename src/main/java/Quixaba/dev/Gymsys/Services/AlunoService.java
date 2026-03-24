0e6e26b5f98759648089af060b6222ac2bcd7c9b
savioquixaba <savioquixaba1@hotmail.com>
2026-03-24 14:10:13 -0300

    refactor: refatora metodo de listar todos os alunos, para receber o DTO e mapear o objeto com Stream

--- src/main/java/Quixaba/dev/Gymsys/Services/AlunoService.java
+++ src/main/java/Quixaba/dev/Gymsys/Services/AlunoService.java
@@ -1,18 +1,23 @@
 package Quixaba.dev.Gymsys.Services;
 
+import Quixaba.dev.Gymsys.DTO.AlunoDTO;
+import Quixaba.dev.Gymsys.Mapper.AlunoMapper;
 import Quixaba.dev.Gymsys.Models.AlunoModel;
 import Quixaba.dev.Gymsys.Repository.AlunoRepository;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class AlunoService {
 
-    private AlunoRepository alunoRepository;
+    private final AlunoMapper alunoMapper;
+    private final AlunoRepository alunoRepository;
 
-    public AlunoService (AlunoRepository alunoRepository ){
+    public AlunoService (AlunoRepository alunoRepository, AlunoMapper alunoMapper){
         this.alunoRepository = alunoRepository;
+        this.alunoMapper = alunoMapper;
     }
 
     //Cria metodo para salvar o Aluno.falta validações
@@ -21,8 +26,10 @@ public class AlunoService {
         return aluno;
     }
 
-    public List<AlunoModel> listarTodosAlunos(){
+    public List<AlunoDTO> listarTodosAlunos(){
         List<AlunoModel> alunos = alunoRepository.findAll();
-        return alunos;
+        return alunos.stream()
+                .map(aluno -> alunoMapper.mapToDTO(aluno))
+                .collect(Collectors.toList());
     }
 }
