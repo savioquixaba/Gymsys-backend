f78ad53a172c3c5002d4c5f40dfde277a420d025
savioquixaba <savioquixaba1@hotmail.com>
2026-04-01 14:36:41 -0300

    feat: criado metodo para criar um plano

---
+++ src/main/java/Quixaba/dev/Gymsys/Services/PlanoService.java
@@ -0,0 +1,27 @@
+package Quixaba.dev.Gymsys.Services;
+
+
+import Quixaba.dev.Gymsys.DTO.PlanoDTO;
+import Quixaba.dev.Gymsys.Mapper.PlanoMapper;
+import Quixaba.dev.Gymsys.Models.PlanoModel;
+import Quixaba.dev.Gymsys.Repository.ContratacaoRepository;
+import Quixaba.dev.Gymsys.Repository.PlanoRepository;
+
+public class PlanoService {
+
+    private PlanoMapper planoMapper;
+    private PlanoRepository planoRepository;
+    private ContratacaoRepository contratacaoRepository;
+
+    public PlanoService(PlanoMapper planoMapper, PlanoRepository planoRepository, ContratacaoRepository contratacaoRepository) {
+        this.planoMapper = planoMapper;
+        this.planoRepository = planoRepository;
+        this.contratacaoRepository = contratacaoRepository;
+    }
+
+    public PlanoDTO criarPlano(PlanoDTO planoDTO){
+        PlanoModel plano = planoMapper.mapToModel(planoDTO);
+        PlanoModel salvo = planoRepository.save(plano);
+        return planoMapper.mapToDto(salvo);
+    }
+}
