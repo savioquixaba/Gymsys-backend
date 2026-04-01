9f9a2ca2c1df5c0e371ae98260229a95b05758a1
savioquixaba <savioquixaba1@hotmail.com>
2026-04-01 11:52:43 -0300

    feat: criado Mapper da entidade Plano X DTO

---
+++ src/main/java/Quixaba/dev/Gymsys/Mapper/PlanoMapper.java
@@ -0,0 +1,28 @@
+package Quixaba.dev.Gymsys.Mapper;
+
+import Quixaba.dev.Gymsys.DTO.PlanoDTO;
+import Quixaba.dev.Gymsys.Models.PlanoModel;
+
+public class PlanoMapper {
+
+    public PlanoModel mapToModel (PlanoDTO planoDTO){
+        PlanoModel planoModel = new PlanoModel();
+
+        planoModel.setPreco(planoDTO.getPreco());
+        planoModel.setDuracaoDias(planoDTO.getDuracaoDias());
+        planoModel.setTipoPlano(planoDTO.getTipoPlano());
+
+        return planoModel;
+    }
+
+    public PlanoDTO mapToDto(PlanoModel planoModel){
+        PlanoDTO planoDTO = new PlanoDTO();
+
+        planoDTO.setId(planoModel.getId());
+        planoDTO.setPreco(planoModel.getPreco());
+        planoDTO.setTipoPlano(planoModel.getTipoPlano());
+        planoDTO.setDuracaoDias(planoModel.getDuracaoDias());
+
+        return planoDTO;
+    }
+}
