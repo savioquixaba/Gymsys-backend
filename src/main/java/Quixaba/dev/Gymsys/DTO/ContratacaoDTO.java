ddeeb62118d1cf58861902081a681fb41b5aa8c8
savioquixaba <savioquixaba1@hotmail.com>
2026-04-02 10:15:04 -0300

    chore: renomeado arquivo DTO da entidade contratacao para ficar condizendo com o resto da aplicacao

--- src/main/java/Quixaba/dev/Gymsys/DTO/ContratacaoDTO.java
+++ src/main/java/Quixaba/dev/Gymsys/DTO/ContratacaoDTO.java
@@ -9,7 +9,7 @@ import java.time.LocalDate;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-public class ContratacoesDTO {
+public class ContratacaoDTO {
 
     private Long id;
     private LocalDate dataInicio;
