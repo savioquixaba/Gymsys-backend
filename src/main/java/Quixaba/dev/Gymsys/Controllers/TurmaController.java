e95f33e007a64eb29dcc9559c63b9263976e62e9
savioquixaba <savioquixaba1@hotmail.com>
2026-04-08 16:07:21 -0300

    feat: metodo listar turmas no controller

--- src/main/java/Quixaba/dev/Gymsys/Controllers/TurmaController.java
+++ src/main/java/Quixaba/dev/Gymsys/Controllers/TurmaController.java
@@ -2,14 +2,15 @@ package Quixaba.dev.Gymsys.Controllers;
 
 import Quixaba.dev.Gymsys.DTO.TurmaDTO;
 import Quixaba.dev.Gymsys.Services.TurmaService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
+@Slf4j
 @RestController
 @RequestMapping("/turma")
 public class TurmaController {
@@ -29,6 +30,22 @@ public class TurmaController {
             return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
         }catch ( Exception e){
             return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(e.getMessage());
+        }
+    }
+
+    @GetMapping("/listar")
+    public ResponseEntity<List<TurmaDTO>> listarTurmas(){
+        try {
+           List<TurmaDTO> lista = turmaService.listarTodasTurmas();
+           if (lista.isEmpty()){
+               log.error("Lista vazia");
+               return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
+           }
+            log.info("lista de alunos: {}",lista);
+           return ResponseEntity.status(HttpStatus.OK).body(lista);
+        }catch (Exception e) {
+            log.error("erro no Servidor", e);
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
         }
     }
 
