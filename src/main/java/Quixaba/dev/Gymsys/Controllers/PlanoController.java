8a6d48e477420d277e0ac55dda5a0786b5762731
savioquixaba <savioquixaba1@hotmail.com>
2026-04-09 15:34:05 -0300

    feat: cria controller e metodo para criar Plano

---
+++ src/main/java/Quixaba/dev/Gymsys/Controllers/PlanoController.java
@@ -0,0 +1,35 @@
+package Quixaba.dev.Gymsys.Controllers;
+
+import Quixaba.dev.Gymsys.DTO.PlanoDTO;
+import Quixaba.dev.Gymsys.Services.PlanoService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RestController
+@RequestMapping("/plano")
+public class PlanoController {
+
+    private PlanoService planoService;
+
+    public PlanoController(PlanoService planoService){
+        this.planoService = planoService;
+    }
+
+    public ResponseEntity<PlanoDTO> criarPlano(PlanoDTO planoDTO){
+        try {
+            PlanoDTO novoPlano = planoService.criarPlano(planoDTO);
+            return ResponseEntity.status(HttpStatus.OK).body(novoPlano);
+        }catch (RuntimeException e){
+            log.error("alguma informação na criação do plano está errada;");
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
+        }catch ( Exception e){
+            log.error("erro de servidor");
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+        }
+
+    }
+}
