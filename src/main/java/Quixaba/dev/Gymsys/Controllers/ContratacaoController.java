f07f1bdb7c86f5cbe5975038f1735e5253a45262
savioquixaba <savioquixabaone@gmail.com>
2026-04-10 21:18:20 -0300

    feat: cria GETMapping para listar as contratacoes

--- src/main/java/Quixaba/dev/Gymsys/Controllers/ContratacaoController.java
+++ src/main/java/Quixaba/dev/Gymsys/Controllers/ContratacaoController.java
@@ -2,13 +2,14 @@ package Quixaba.dev.Gymsys.Controllers;
 
 import Quixaba.dev.Gymsys.DTO.ContratacaoDTO;
 import Quixaba.dev.Gymsys.Services.ContratacaoService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
+@Slf4j
 @RestController
 @RequestMapping("/contratacao")
 public class ContratacaoController {
@@ -27,6 +28,21 @@ public class ContratacaoController {
         }catch (RuntimeException e){
             return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
         }catch (Exception e){
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+        }
+    }
+
+    @GetMapping("/listar")
+    public ResponseEntity<List<ContratacaoDTO>> listar(){
+        try {
+            List<ContratacaoDTO> lista = contratacaoService.listarContratacao();
+            log.info("Lista: {}", lista);
+            return ResponseEntity.status(HttpStatus.OK).body(lista);
+        }catch (RuntimeException e){
+            log.info("Não existe informações a serem listadas");
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+        }catch (Exception e){
+            log.error("Erro de servidor");
             return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
         }
     }
