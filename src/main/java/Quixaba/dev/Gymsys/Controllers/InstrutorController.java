5f88a0afbbee519a92c693861c582c868dcac47f
savioquixaba <savioquixaba1@hotmail.com>
2026-04-13 15:03:00 -0300

    feat: cria metodo para listar todas os instrutores

--- src/main/java/Quixaba/dev/Gymsys/Controllers/InstrutorController.java
+++ src/main/java/Quixaba/dev/Gymsys/Controllers/InstrutorController.java
@@ -2,13 +2,14 @@ package Quixaba.dev.Gymsys.Controllers;
 
 import Quixaba.dev.Gymsys.DTO.InstrutorDTO;
 import Quixaba.dev.Gymsys.Services.InstrutorService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
+@Slf4j
 @RestController
 @RequestMapping("/instrutor")
 public class InstrutorController {
@@ -27,6 +28,19 @@ public class InstrutorController {
         } catch (RuntimeException e) {
             return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
         } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+        }
+    }
+
+    @GetMapping("/listar")
+    public ResponseEntity<List<InstrutorDTO>> listar(){
+        try {
+            List<InstrutorDTO> lista = instrutorService.listarTodos();
+            return ResponseEntity.status(HttpStatus.OK).body(lista);
+        } catch (RuntimeException e) {
+            log.error("Não há dados a serem listados.");
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+        }catch (Exception e){
             return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
         }
     }
